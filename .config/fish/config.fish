e1f491f8bd0a7c187385391d5c791cac19acbca4
octav <zahariao@gmail.com>
2019-03-04 17:49:28 +0200

    Fix fish init

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,10 +4,17 @@ function i3cheatsheet
   grep -E "^bindsym" ~/.config/i3/config | awk '{$1=""; print $0}' | sed 's/^ *//g' | grep -vE "^XF86" | column | pr -2 -w 160 -t | less
 end
 
-export KAFKA_HOME='/home/ozaharia/apps/confluent-3.3.0'
-export SPARK_HOME='/home/ozaharia/apps/spark-2.3.1'
+source /home/ozaharia/.oh-my-zsh/env.sh
 
-bindkey -v
+set -x KAFKA_HOME /home/ozaharia/apps/confluent-3.3.0
+set -x  SPARK_HOME /home/ozaharia/apps/spark-2.3.1
+set -x IDEA_JDK /home/ozaharia/bin/jdk-idea
+set -x TERM xterm-color
+set -x LD_LIBRARY_PATH /usr/local/lib
+set -x GOROOT /usr/local/go
+set -x PATH $PATH $GOPATH/bin $GOROOT/bin /home/ozaharia/go/bin /home/ozaharia/.cargo/bin /home/ozaharia/.local/bin /home/ozaharia/bin /home/ozaharia/.cabal/bin /home/ozaharia/node_modules/.bin /home/ozaharia/apps/apache-maven-3.6.0/bin
+
+
 alias work="cd ~/work"
 alias p="cd ~/projects"
 alias tmp="cd ~/tmp"
@@ -21,23 +28,18 @@ alias aw="vim ~/.config/awesome/rc.lua"
 alias reload="source ~/.config/fish/config.fish"
 alias rc="vim ~/.config/fish/config.fish"
 alias dow="cd ~/Downloads"
-alias back='feh --bg-scale "/home/ozaharia/Pictures/$((RANDOM%60)).jpg"'
+alias back='feh --bg-scale "/home/ozaharia/Pictures/(random 1 60).jpg"'
 alias ll='ls -al'
 alias cal='ncal -3Mb'
-alias inline='cd /home/ozaharia/projects/inline/haskell && ls -al'
-alias kc='cd /home/ozaharia/projects/scala/kclient && ls -al'
-alias ovpn="oathtool --base32 --totp $OSECRET | xclip -sel clip && sudo openvpn  --config ~/.openvpn/client.ovpn"
+alias inline='cd /home/ozaharia/projects/inline/haskell and ls -al'
+alias kc='cd /home/ozaharia/projects/scala/kclient and  ls -al'
+alias ovpn="oathtool --base32 --totp $OSECRET | xclip -sel clip ;sudo openvpn --config ~/.openvpn/client.ovpn"
 alias s='sbt -mem 4000'
 alias sd='sbt -mem 4000 -jvmDebug 9999'
 
 setxkbmap us -variant colemak
-bindkey '^R' history-incremental-search-backward
 
-alias start-kafka="($KAFKA_HOME/bin/zookeeper-server-start $KAFKA_HOME/etc/kafka/zookeeper.properties &  sleep 3 && $KAFKA_HOME/bin/kafka-server-start $KAFKA_HOME/etc/kafka/server.properties &)"
 #xinput set-float-prop 'DLL07D1:01 044E:120B' 'Device Accel Constant Deceleration' 4  
-
-export GOROOT=/usr/local/go
-export PATH=$PATH:$GOPATH/bin:$GOROOT/bin:/home/ozaharia/go/bin:/home/ozaharia/.cargo/bin:/home/ozaharia/.local/bin:/home/ozaharia/bin:/home/ozaharia/.cabal/bin:/home/ozaharia/node_modules/.bin:/home/ozaharia/apps/apache-maven-3.6.0/bin
 
 source ~/.xsessionrc
 #setxkbmap -option ctrl:nocaps
@@ -45,11 +47,8 @@ source ~/.xsessionrc
 #xcape -e 'Control_L=Escape'
 #xmodmap -e 'keycode 77 = NoSymbol Num_Lock'
 
-export IDEA_JDK=~/bin/jdk-idea
 source ~/work/env.sh
-export TERM=xterm-color
 synclient MaxTapTime=0
-export LD_LIBRARY_PATH=/usr/local/lib
 
 alias aws_whoami='aws iam list-account-aliases'
 fish_vi_key_bindings
